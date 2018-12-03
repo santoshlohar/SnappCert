60c2774553f30d640c03c09f48bd4b54e4c1e7ef
SushmitaSnapper <sushmita.pundir@snapperfuturetech.com>
2018-12-03 18:24:02 +0530

    createid screen and other screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { ReviewMarksheetDataComponent } from './review-marksheet-data/review-mar
 import { UploadMarksheetDataComponent } from './upload-marksheet-data/upload-marksheet-data.component';
 import { ReviewCertificateDataComponent } from './review-certificate-data/review-certificate-data.component';
 import { UploadCertificateDataComponent } from './upload-certificate-data/upload-certificate-data.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +49,9 @@ import { UploadCertificateDataComponent } from './upload-certificate-data/upload
     ReviewMarksheetDataComponent,
     UploadMarksheetDataComponent,
     ReviewCertificateDataComponent,
-    UploadCertificateDataComponent
+    UploadCertificateDataComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/create-id/create-id.component.ts
+++ src/app/create-id/create-id.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-id',
-  templateUrl: './create-id.component.html',
-  styleUrls: ['./create-id.component.css']
+	selector: 'app-create-id',
+	templateUrl: './create-id.component.html',
+	styleUrls: ['./create-id.component.css']
 })
 export class CreateIdComponent implements OnInit {
+	createIdForm: any = {};
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	createId(form: NgForm) {
+		this.createIdForm.emailId = form.value.email;
+		this.createIdForm.password = form.value.password;
+		this.createIdForm.confirmPassword = form.value.confirmPassword; 
+		console.log(this.createIdForm);
+	}
 }
