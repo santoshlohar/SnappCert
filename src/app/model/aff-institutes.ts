a7d590aab9075e152c25f221bd45f6ffcd0a6914
SushmitaSnapper <sushmita.pundir@snapperfuturetech.com>
2019-01-21 11:54:54 +0530

    remove old_app code

--- src/app/model/aff-institutes.ts
+++
@@ -1,8 +0,0 @@
-export interface AffInstitutes {
-    Institution_ID: string;
-    department_ID: string;
-    AfflInstitution_ID: string;
-    afflInstitute_Name: string;
-    afflInstitute_loc: string;
-    _id: number;
-}
--- src/app/model/institutes-depts.ts
+++
@@ -1,9 +0,0 @@
-export interface InstituteDepts {
-    position: number;
-    instituteId: string;
-    instituteName: string;
-    deptId: string;
-    deptName: string;
-    status: string;
-    _id: string;
-}  
