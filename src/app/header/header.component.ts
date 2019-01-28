7cfeb6b468995754c8566ecd0c1a843c241604d6
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-01-28 17:57:00 +0530

    Login popup functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,7 +23,7 @@ export class HeaderComponent implements OnInit {
 
 	logout() {
 		this.authService.logout();
-		this.router.navigate(['/login']);
+		this.router.navigate(['']);
 	}
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
+	userLogin: FormGroup;
+	loginData: any = {};
+	url: string;
+	userData: any;
+	type: string;
+
 	constructor(public dialogRef: MatDialogRef<LoginComponent>,
-				@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+				
+				private formBuilder: FormBuilder,
+				private authService: AuthService,
+				private router: Router) {}
 
 	ngOnInit() {
+		this.userLogin = this.formBuilder.group({
+			emailId: (['', Validators.required]),
+			password: (['', Validators.required])
+		});
 	}
 
 	onNoClick(): void {
@@ -25,7 +37,32 @@ export class LoginComponent implements OnInit {
 	}
 
 	login(form: NgForm) {
-		console.log(form);
+		this.loginData.emailId = form.value.emailId;
+		this.loginData.password = form.value.password;
+		 
+		this.authService.login(this.loginData)
+			.subscribe((response) => {
+				this.userData = response;
+				this.type = this.userData.UserType;
+
+				if( this.type == 'KYC_AGENT' ) {
+					this.router.navigate(['/','institutes']);
+				} else if (this.type == 'INST_ADMIN') {
+					this.router.navigate(['/','departments']);	
+				} else if (this.type == 'INS_DATA_MANAGER') {
+					this.router.navigate(['/','courses']);	
+				}
+
+				this.dialogRef.close();
+			},
+			(error) => {
+				console.log(error);
+			}
+			)
+	};
+
+	close() {
+		this.dialogRef.close();
 	}
 
 }
