c4cd1cc822fc1a86d9e6235c21761c8352da4e27
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-01-29 12:44:04 +0530

    uploaded certificate data list page

--- src/app/certificates/certificate-upload-list/certificate-upload-list.component.ts
+++ src/app/certificates/certificate-upload-list/certificate-upload-list.component.ts
@@ -1,28 +1,155 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatSort, MatTableDataSource, MatPaginator, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { UploadedCertificates } from 'src/app/modals/uploaded_certificate';
 
 @Component({
-  selector: 'app-certificate-upload-list',
-  templateUrl: './certificate-upload-list.component.html',
-  styleUrls: ['./certificate-upload-list.component.css']
+	selector: 'app-certificate-upload-list',
+	templateUrl: './certificate-upload-list.component.html',
+	styleUrls: ['./certificate-upload-list.component.css']
 })
 export class CertificateUploadListComponent implements OnInit {
+	
+	displayedColumns = ['instituteId', 'affiliatedInstituteId', 'courseId', 'batchId', 'studentId', 'certificateId', 'specialization', 'scoreEarned', 'totalScore', 'cgpa', 'creditsEarned', 'completionDate', 'transactionStatus', 'failureReason', 'transactionMachine', 'transactionDate', 'transactionTime', 'transactionUser'];
+	url: string;
 
-  displayedColumns = ['instituteId', 'affiliatedInstituteId', 'courseId', 'batchId', 'studentId', 'certificateId', 'specialization', 'scoreEarned', 'totalScore', 'cgpa', 'creditsEarned', 'completionDate', 'transactionStatus', 'failureReason', 'transactionMachine', 'transactionDate', 'transactionTime', 'transactionUser', '_id'];
-  url: string;
-  
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+	dataSource = new MatTableDataSource<UploadedCertificates>(certificate_data);
+	selection = new SelectionModel<UploadedCertificates>(true, []);
 
-  constructor(private apiService: ApiService,
-              public dialoge: MatDialog,
-              public router: Router) { }
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit() {
-    
-  }
+	constructor(private apiService: ApiService,
+				public dialoge: MatDialog,
+				public router: Router) { }
 
+	ngOnInit() {
+		this.dataSource.sort = this.sort;
+		this.dataSource.paginator = this.paginator;
+	}
 }
+
+const certificate_data:  UploadedCertificates[] = [
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	},
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	},
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	},
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	},
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	},
+	{
+		Institution_ID: '1',
+		Affiliated_Institute_ID: 'AI01',
+		Course_ID: 'CI01',
+		Batch_ID: '001',
+		Student_ID: '001',
+		Certificate_ID: 3627,
+		Specialization: "computer application",
+		Score_Earned: 786,
+		Total_Score: 1000,
+		CGPA: 6,
+		Credits_Earned: 6,
+		Completion_Date: '12Jan',
+		Transaction_Status: 'New',
+		Failure_Reason: 'Data not correct',
+		Transaction_Machine: '84392',
+		Transaction_Date: '29 Jan',
+		Transaction_Time: '15 Hrs',
+		Transaction_User: 'Sushmita'
+	}
+];
---
+++ src/app/modals/uploaded_certificate.ts
@@ -0,0 +1,20 @@
+export interface UploadedCertificates {
+    Institution_ID: string;
+    Affiliated_Institute_ID: string;
+    Course_ID: string;
+    Batch_ID: string;
+    Student_ID: string;
+    Certificate_ID: number;
+    Specialization: string;
+    Score_Earned: number;
+    Total_Score: number;
+    CGPA: number;
+    Credits_Earned: number;
+    Completion_Date: string;
+    Transaction_Status: string;
+    Failure_Reason: string;
+    Transaction_Machine: string;
+    Transaction_Date: string;
+    Transaction_Time: string;
+    Transaction_User: string;
+}
