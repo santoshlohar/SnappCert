8738219c89ce2c4d5e06551e2fa053f43748e4d4
SushmitaSnapper <sushmita.pundir@snapperfuturetech.com>
2019-02-08 00:35:34 +0530

    some changes on person list

--- src/app/certificates/person-list/person-list.component.ts
+++ src/app/certificates/person-list/person-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TableDataSource, ValidatorService } from 'angular4-material-table';
 import { PersonValidatorsService } from './person-validators.service';
 import { Person } from './person';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material';
 
 @Component({
@@ -17,34 +17,41 @@ export class PersonListComponent implements OnInit {
 
 	tableForm: FormGroup;
 	displayedColumns = ['name', 'age', 'actionsColumn'];
+	name = new FormControl();
+	age = new FormControl();
+
+	
+	personListChange = new EventEmitter<Person[]>();
+
+	dataSource = new TableDataSource<Person[]>(Element_Data);
 
 	constructor(private personValidator: ValidatorService,
-		private fb: FormBuilder) { }
+		private fb: FormBuilder) { 
+			this.tableForm = this.fb.group({
+				name: [{value: ''}, Validators.required],
+				age: [{value: ''}, Validators.required],
+			});
+		}
 	
-
-	@Input() personList = [ 
-		{ name: 'Mark', age: 15 },
-		{ name: 'Brad', age: 50 },
-		] ;
-	@Output() personListChange = new EventEmitter<Person[]>();
-
-	// dataSource: MatTableDataSource<Person[]>;
-	dataSource = new MatTableDataSource<Person>(this.personList);
+	//dataSource = new MatTableDataSource<Person>(this.personList);
 
 	ngOnInit() {
-		this.tableForm = this.fb.group({
-			name: [{value: ''}, Validators.required],
-			age: [{value: ''}, Validators.required],
-		});
-		
+		console.log(this.dataSource);
 		// this.dataSource = new TableDataSource<any>(this.personList, Person, this.personValidator);
 
 		// this.dataSource.datasourceSubject.subscribe(personList => this.personListChange.emit(personList));
 	}
 
-	addData() {
-		this.dataSource.data = this.personList;
-	}
+	// addData() {
+	// 	this.dataSource.data = this.personList;
+	// }
 	
 
 }
+
+const Element_Data: Person[] = [
+	
+	{ name: 'Mark', age: 15 },
+	{ name: 'Brad', age: 50 },
+
+]
