c17540984bbc3caea67fcfbfd4d69d8b133697da
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-02-16 10:40:30 +0530

    add batch upload list component

---
+++ src/app/batches/batch-upload-list/batch-upload-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-upload-list',
+  templateUrl: './batch-upload-list.component.html',
+  styleUrls: ['./batch-upload-list.component.css']
+})
+export class BatchUploadListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
