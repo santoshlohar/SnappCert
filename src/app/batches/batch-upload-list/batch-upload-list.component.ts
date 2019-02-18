d6102ec919350c22866529e29b77dd2e4e91f965
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-02-18 15:12:31 +0530

    batch upload API integration

--- src/app/batches/batch-upload-list/batch-upload-list.component.ts
+++ src/app/batches/batch-upload-list/batch-upload-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-batch-upload-list',
@@ -12,14 +13,27 @@ export class BatchUploadListComponent implements OnInit {
 	url;
 	loginUser;
 	authUsers: [] = [];
+	uploadedFileName;
 	displayedColumns = [
-		'userType', 
 		'instituteId', 
-		'departmentId', 
-		'affInstituteId', 
-		'username', 
-		'emailId', 
-		'phone', 
+		'affiliated', 
+		'courseId', 
+		'batchId', 
+		'batchYear', 
+		'minCredits', 
+		'minCgpa',
+		'totalCgpa',
+		'minScore',
+		'totalScore',
+		'termType',
+		'termId',
+		'termStart',
+		'termEnd',
+		'DataStatus',
+		'comments',
+		'date',
+		'time',
+		'userName',
 		'_id'
 	];
 
@@ -29,9 +43,63 @@ export class BatchUploadListComponent implements OnInit {
 	@ViewChild(MatSort) sort: MatSort;
 	@ViewChild(MatPaginator) paginator: MatPaginator;
 
-	constructor() { }
+	constructor(private apiService: ApiService) { }
 
 	ngOnInit() {
+		//this.dataSource.data = Batch_Data;
+		//console.log(this.dataSource.data);
+	}
+
+	uploadBatch(files, filename) {
+		var form = new FormData();
+		form.append(filename, files[0]);
+
+		this.url = '/batchdata/fileupload';
+		this.apiService.post(this.url, form)
+			.subscribe((response) => {
+				console.log(response);
+			},
+			(error) => {
+				console.log(error);
+			});
+		
+	}
+
+	getTempBatch(){
+		this.url = '/temp/batchdata';
+
+		this.apiService.get(this.url)
+			.subscribe((response) => {
+				console.log(response);
+			},
+			(error) => {
+				console.log(error);
+			})
 	}
 
 }
+
+// const Batch_Data: any[] = [
+// 	{
+// 		instituteId: '111', 
+// 		affiliated: '111', 
+// 		courseId: '11', 
+// 		batchId:'11', 
+// 		batchYear:'11', 
+// 		minCredits:'11', 
+// 		minCgpa:'aaa',
+// 		totalCgpa:'aa',
+// 		minScore:'aa',
+// 		totalScore:'aa',
+// 		termType:'aa',
+// 		termId: 'aa',
+// 		termStart:'aa',
+// 		termEnd:'aa',
+// 		dataStatus:'aa',
+// 		comments:'aa',
+// 		date:'aa',
+// 		time:'aa',
+// 		userName:'aa',
+// 		_id: 'aa'
+// 	}
+// ]
