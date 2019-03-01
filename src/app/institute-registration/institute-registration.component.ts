e789a6559183a0bd14b96e41404f22d8ad3c0771
nehaprashantmutke <neha.mutke@snapperfuturetech.com>
2019-03-01 11:21:15 +0530

    register institute

--- src/app/institute-registration/institute-registration.component.ts
+++ src/app/institute-registration/institute-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs';
 
 @Component({
@@ -9,16 +10,11 @@ import 'rxjs';
 })
 export class InstituteRegistrationComponent implements OnInit {
 
-	intReg: any = {
-		requester_Name: '', requester_Email: '', requester_Phone: '', inst_Type: '', institute_ID: '', institute_Name: '', establish_Date: '', 
-		address_1: '', address_2: '', State: '', City: '', academic_Head: '', academic_Head_Email: '', academic_Head_Phone: '', administrator_Head: '',
-		administrator_Head_Email: '', administrator_Head_Phone:'', boardLine_No: '', Location: '', Website: '', affiliated_To: '', affiliated_To_Type: '',
-		recognized_By: '', regulatory_Body: '', Name: '', Email: '', Phone: ''
-	};
 	url;
 	instRequestForm: FormGroup;
 
-	constructor(private _formBuilder: FormBuilder, 
+	constructor(private http: HttpClient,
+				private _formBuilder: FormBuilder, 
 		) { }
 
 	ngOnInit() {
@@ -62,36 +58,46 @@ export class InstituteRegistrationComponent implements OnInit {
 		if (regForm.invalid) {
 			return;
 		}
-		this.url = '/registration';
 
-		this.intReg.requester_Name = regForm.value.requesterName;
-		this.intReg.requester_Email = regForm.value.requesterEmail;
-		this.intReg.requester_Phone = regForm.value.requesterPhone;
-		this.intReg.inst_Type = regForm.value.instType;
-		this.intReg.institute_ID = regForm.value.instituteID;
-		this.intReg.institute_Name = regForm.value.instituteName;
-		this.intReg.establish_Date = regForm.value.establishDate;
-		this.intReg.address_1 = regForm.value.address1;
-		this.intReg.address_2 = regForm.value.address2;
-		this.intReg.State = regForm.value.state;
-		this.intReg.City = regForm.value.city;
-		this.intReg.academic_Head = regForm.value.academicHead;
-		this.intReg.academic_Head_Email = regForm.value.academicHeadEmail;
-		this.intReg.academic_Head_Phone = regForm.value.academicHeadPhone;
-		this.intReg.administrator_Head = regForm.value.administratorHead;
-		this.intReg.administrator_Head_Email = regForm.value.administratorHeadEmail;
-		this.intReg.administrator_Head_Phone = regForm.value.administratorHeadPhone;
-		this.intReg.boardLine_No = regForm.value.boardLineNo;
-		this.intReg.Location = regForm.value.location;
-		this.intReg.Website = regForm.value.website;
-		this.intReg.affiliated_To = regForm.value.affiliatedTo;
-		this.intReg.affiliated_To_Type = regForm.value.affiliatedToType;
-		this.intReg.recognized_By = regForm.value.recognizedBy;
-		this.intReg.regulatory_Body = regForm.value.regulatoryBody;
-		this.intReg.Name = regForm.value.name;
-		this.intReg.Email = regForm.value.email;
-		this.intReg.Phone = regForm.value.phone;
-		console.log(this.intReg);
+		var data = {
+			requesterName : regForm.value.requesterName,
+			requesteremailId : regForm.value.requesterEmail,
+			requesterphoneNo : regForm.value.requesterPhone,
+			instituteName : regForm.value.instituteName,
+			instituteType : regForm.value.instType,
+			instituteId : regForm.value.instituteID,
+			establishmentDate : regForm.value.establishDate,
+			address1 : regForm.value.address1,
+			address2 : regForm.value.address2,
+			state : regForm.value.state,
+			city : regForm.value.city,
+			academicHeadName : regForm.value.academicHead,
+			academicHeadEmailId : regForm.value.academicHeadEmail,
+			academicHeadPhone : regForm.value.academicHeadPhone,
+			administratorName : regForm.value.administratorHead,
+			administratorEmailId : regForm.value.administratorHeadEmail,
+			administratorPhone : regForm.value.administratorHeadPhone,
+			boardLineNumber : regForm.value.boardLineNo,
+			location : regForm.value.location,
+			website : regForm.value.website,
+			affiliatedTo : regForm.value.affiliatedTo,
+			affiliatedInstituteType : regForm.value.affiliatedToType,
+			recognizedBy : regForm.value.recognizedBy,
+			regulatoryBodyName : regForm.value.regulatoryBody,
+			// kycStatus : "NEW",
+			InstAdminName : regForm.value.name,
+			InstAdminEmailId : regForm.value.email,
+			InstAdminPhoneNo : regForm.value.phone
+		}
+		
+		this.url = 'http://localhost:3000/api/v1/registerInstitute';
+
+		this.http.post(this.url, data)
+			.subscribe((response: any) => {
+				if(response.message == 'success') {
+					alert('Success');
+				}
+			});
 	}
 	
 
