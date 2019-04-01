2bc004706820b50744bb24277a45f95d683b855d
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-04-01 12:02:06 +0530

    user model confict fix

--- src/app/modals/user.ts
+++ src/app/modals/user.ts
@@ -1,11 +1,14 @@
 export interface User { 
-    userType: String;
+    UserType: String;
     instituteId: String;
     departmentId: String;
     affInstituteId: String;
     username: String;
     emailId: String;
     phone: String;
+    isActivated: Boolean;
+    activated: string;
+    token?: string;
     status: String;
     _id: Number;
 }
