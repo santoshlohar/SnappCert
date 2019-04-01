8e7abd05c95ac72837d6b8631c551e1a15227495
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-04-01 12:26:41 +0530

    Code formating

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -5,72 +5,72 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-user-edit',
-  templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+	selector: 'app-user-edit',
+	templateUrl: './user-edit.component.html',
+	styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
 
-  url;
-  id;
-  inst_id;
-  affInst_Id
-  loginUser;
-  user;
-  role;
-  departments:[]=[];
-  UserData = {
-    UserType: '',
-    department_ID:'',
-    UserName: '',
-    emailId: '',
-    phoneNumber: '',
-  };
-  editUserForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,
-              private apiService: ApiService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private location: Location) { 
-              }
+	url;
+	id;
+	inst_id;
+	affInst_Id
+	loginUser;
+	user;
+	role;
+	departments:[]=[];
+	UserData = {
+	UserType: '',
+	department_ID:'',
+	UserName: '',
+	emailId: '',
+	phoneNumber: '',
+	};
+	editUserForm: FormGroup;
+	constructor(private formBuilder: FormBuilder,
+				private apiService: ApiService,
+				private route: ActivatedRoute,
+				private router: Router,
+				private location: Location) { 
+				}
 
-  ngOnInit() {
-    this.id = this.route.snapshot.params['userId'];
-    this.loginUser = JSON.parse(localStorage.getItem('user'));
-    this.role = this.loginUser.UserType;
-    this.inst_id = this.loginUser.instituteID;
-    if (this.loginUser.Affliated_Institute_ID != '') {
-      this.affInst_Id = this.loginUser.Affliated_Institute_ID;
-    }
-    this.getUserById(this.id);
-    this.getDeptList();
-    this.editUserForm = this.formBuilder.group({
-      UserType: ['', Validators.required],
-      department_ID: [''],
-      UserName: ['', Validators.required],
-      emailId: ['', Validators.required],
-      phoneNumber: ['', Validators.required]
-    });
-  }
+	ngOnInit() {
+		this.id = this.route.snapshot.params['userId'];
+		this.loginUser = JSON.parse(localStorage.getItem('user'));
+		this.role = this.loginUser.UserType;
+		this.inst_id = this.loginUser.instituteID;
+		if (this.loginUser.Affliated_Institute_ID != '') {
+			this.affInst_Id = this.loginUser.Affliated_Institute_ID;
+		}
+		this.getUserById(this.id);
+		this.getDeptList();
+		this.editUserForm = this.formBuilder.group({
+			UserType: ['', Validators.required],
+			department_ID: [''],
+			UserName: ['', Validators.required],
+			emailId: ['', Validators.required],
+			phoneNumber: ['', Validators.required]
+		});
+	}
 
-  getUserById(id) {
-    this.url = "/user/";
-    console.log(this.url);
-    var params = '';
-    this.apiService.get(this.url+id, params)
-        .subscribe((response) => {
-          if (response.message == 'success' && response.data != '') {
-            this.user = response.data;
-            console.log("this.user: " + JSON.stringify(this.user));
-            this.editUserForm.patchValue(this.user);
-          }
-        },
-        (error) => {
-          console.log(error);
-        });
-  }
+	getUserById(id) {
+		this.url = "/user/";
+		console.log(this.url);
+		var params = '';
+		this.apiService.get(this.url+id, params)
+			.subscribe((response) => {
+				if (response.message == 'success' && response.data != '') {
+					this.user = response.data;
+					console.log("this.user: " + JSON.stringify(this.user));
+					this.editUserForm.patchValue(this.user);
+				}
+			},
+			(error) => {
+				console.log(error);
+			});
+	}
 
-  getDeptList() {
+	getDeptList() {
 		this.url = "/departmentByInst/";
 		var params = '';
 		this.apiService.get(this.url+ this.inst_id, params)
@@ -86,30 +86,30 @@ export class UserEditComponent implements OnInit {
 			})
 	}
 
-  editUser(data: NgForm) {
-    this.url = "/user/";
-    this.UserData.UserType = data.value.UserType;
-    this.UserData.department_ID = data.value.department_ID;
-    this.UserData.UserName = data.value.UserName;
-    this.UserData.emailId = data.value.emailId;
-    this.UserData.phoneNumber = data.value.phoneNumber;
+	editUser(data: NgForm) {
+		this.url = "/user/";
+		this.UserData.UserType = data.value.UserType;
+		this.UserData.department_ID = data.value.department_ID;
+		this.UserData.UserName = data.value.UserName;
+		this.UserData.emailId = data.value.emailId;
+		this.UserData.phoneNumber = data.value.phoneNumber;
 
-    this.apiService.put(this.url+this.id, this.UserData)
-        .subscribe((response) => {
-          if (response.message == 'success') {
-            this.router.navigate(['/users']);
-          } else {
-            var errmsg = response.error.msg;
-            alert(errmsg);
-          }
-        },
-        (error) => {
-          console.log(error);
-        });
-  }
+		this.apiService.put(this.url+this.id, this.UserData)
+			.subscribe((response) => {
+				if (response.message == 'success') {
+					this.router.navigate(['/users']);
+				} else {
+					var errmsg = response.error.msg;
+					alert(errmsg);
+				}
+			},
+			(error) => {
+				console.log(error);
+			});
+	}
 
-  goBack() {
-    this.location.back();
-  }
+	goBack() {
+		this.location.back();
+	}
 
 }
