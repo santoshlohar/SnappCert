209ec8dc1d283e752214010694ecc26125982106
Sushmita Pundir <sushmita.pundir@snapperfuturetech.com>
2019-05-21 14:50:22 +0530

    final students

--- src/app/students/students-final/students-final.component.ts
+++ src/app/students/students-final/students-final.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+import { ErrorDialogService } from 'src/app/services/error-dialog.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-students-final',
-  templateUrl: './students-final.component.html',
-  styleUrls: ['./students-final.component.css']
+	selector: 'app-students-final',
+	templateUrl: './students-final.component.html',
+	styleUrls: ['./students-final.component.css']
 })
 export class StudentsFinalComponent implements OnInit {
 
-  constructor() { }
+	loggedInUser;
+	role;
+	entity;
+	student;
+	selectedStudents: any = [];
+	displayedColumns = [
+		'batchId', 
+		'code', 
+		'name', 
+		'father',
+		'dob',
+		'aadhar',
+		'email',
+		'phoneNumber',
+		'date',
+		'status'
+	];
 
-  ngOnInit() {
-  }
+	batchIdFilter = new FormControl();
+	studentIdFilter = new FormControl();
+	nameFilter = new FormControl();
+	fatherNameFilter = new FormControl();
+	dobFilter = new FormControl();
+	aadharFilter = new FormControl();
+	emailFilter = new FormControl();
+	phoneNumberFilter = new FormControl();
+	dateFilter = new FormControl();
+	statusFilter = new FormControl();
+
+	filteredValues = {
+		batchId: '',
+		code: '',
+		name: '',
+		father: '',
+		dob: '',
+		aadhar: '',
+		email: '',
+		phoneNumber: '',
+		date: '',
+		status: ''
+	}
+
+	dataSource = new MatTableDataSource<any>(data);
+	selection = new SelectionModel<any>(true, []);
+
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	constructor(public errorDialogService: ErrorDialogService) { }
+
+	ngOnInit() {
+	}
 
 }
+
+
+const data = [{
+	batch: {
+		code: 'BI001'
+	},
+	code: "SI001",
+	name: "Sushmita",
+	father: "Mr. N S Pundir",
+	dob: "03/11/1993",
+	aadhar: "123412341234",
+	email: "sush@gmail.com",
+	phoneNumber: "8433892503",
+	date: "21/05/2019",
+	status: "Active"
+}]
